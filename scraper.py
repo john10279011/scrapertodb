5a629a2d7565be1170cfc7bc248f8233bd0ed324
john <johnmugure659@gmail.com>
2024-04-27 14:57:52 +0300

    headless browser

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 driver.get(url)
 
